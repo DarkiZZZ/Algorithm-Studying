cacc4ecbeb39883a6c78d5ea3c646eac05bdf661
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-13 11:42:53 +0300

    recursion for factorial made

--- BasicRecursion/src/BasicRecursion.java
+++ BasicRecursion/src/BasicRecursion.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class BasicRecursion {
 
     public static void main(String[] args) {
-        System.out.print("Enter the start of countdown: ");
+        System.out.println("Enter the start of countdown: ");
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
         recursion(number);
+
+        //=================================================
+
+        System.out.println("Enter number to count its factorial");
+        int x = scanner.nextInt();
+        System.out.println(factorialRecursion(x));
     }
 
     // print countdown to console from integer "start" to 1
@@ -17,6 +23,15 @@ public class BasicRecursion {
         }
         else {
             recursion(start - 1);
+        }
+    }
+
+    public static int factorialRecursion(int x){
+        if (x == 1){
+            return 1;
+        }
+        else {
+            return x * factorialRecursion(x - 1);
         }
     }
 }
