984611d948dbfc4914bd73528c448658243a90f0
Mike Sokolov <72737016+DarkiZZZ@users.noreply.github.com>
2022-05-13 11:13:32 +0300

    basic recursion made

--- BasicRecursion/src/BasicRecursion.java
+++ BasicRecursion/src/BasicRecursion.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class BasicRecursion {
 
     public static void main(String[] args) {
-        recursion(20);
+        System.out.print("Enter the start of countdown: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        recursion(number);
     }
 
     // print countdown to console from integer "start" to 1
     public static void recursion(int start){
-        System.out.println(start);
+        System.out.print(start + " ");
         if (start <= 1){
             return;
         }
